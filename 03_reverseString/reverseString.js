825648c0c2274ce07c0cceb09f05810f4e3db52a
angelgonzalezn <143057497+angelgonzalezn@users.noreply.github.com>
2023-12-29 15:20:17 -0400

    Complete exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,23 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split('').reverse().join('');
 };
+
+/* Below, a more verbose yet still fully functional approach
+to this function.
+
+const reverseString = function(str) {
+    const letters = [];
+    for (let i = 0; i <= str.length; i++) {
+        letters.push(str.charAt(str.length-i));
+    }
+
+    let reversedText = '';
+    for (const letter of letters) {
+        reversedText += letter;
+    }
+
+    return reversedText;
+}; */
 
 // Do not edit below this line
 module.exports = reverseString;
