c0cfcfa4d43c049b7151049c850384e016c1c3ce
angelgonzalezn <143057497+angelgonzalezn@users.noreply.github.com>
2023-12-29 22:28:13 -0400

    Complete exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,19 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    return arr.filter((value) => !args.includes(value));
+}
 
-};
+/* Below, first version of this function. It passed all tests,
+but it may not work if we need to exclude one value that appears
+multiple times in the array.
+
+const removeFromArray = function(arr, ...args) {
+    for (const value of args) {
+        let index = arr.indexOf(value);
+        if (index < 0) continue;
+        arr.splice(index, 1);
+    }
+    return arr;
+}; */
 
 // Do not edit below this line
 module.exports = removeFromArray;
